d532795970e4de9f542a9003b99180edfc06838e
Zoltan Dalmadi <dmz985@gmail.com>
2014-09-30 20:56:23 +0200

    Reformat Point2D class

--- Utils/Point2D.h
+++ Utils/Point2D.h
@@ -18,144 +18,145 @@ namespace Utils {
     GLfloat size = 10.0;
     bool smooth = true;
 
-    // Initialize point at (0, 0) (origin). (glut: bottom-left corner)
+    /// Initialize point at (0, 0) (origin). (glut: bottom-left corner)
     Point2D() : xp(0), yp(0) {}
 
-    // Initialize with raw coordinates.
+    /// Initialize with raw coordinates.
     Point2D(T x, T y) : xp(x), yp(y) {}
 
-    // Returns true if Point's coordinates are zero.
+    /// Returns true if Point's coordinates are zero.
     inline bool isNull() const {
       return (xp == 0) && (yp == 0);
     }
 
-    // Returns X coordinate.
+    /// Returns X coordinate.
     inline T x() const {
       return xp;
     }
 
-    // Returns Y coordinate.
+    /// Returns Y coordinate.
     inline T y() const {
       return yp;
     }
 
-    // Sets X coordinate.
+    /// Sets X coordinate.
     inline void setX(T value) {
       xp = value;
     }
 
-    // Sets Y coordinate.
+    /// Sets Y coordinate.
     inline void setY(T value) {
       yp = value;
     }
 
-    // Returns a pointer to this Point2D.
+    /// Returns a pointer to this Point2D.
     inline Point2D<T>* getPointer() {
       return this;
     }
 
-    // Returns a reference to this Point2D.
+    /// Returns a reference to this Point2D.
     inline Point2D<T>& getRef() {
       return *this;
     }
 
-    // Compound addition operator overload.
+    /// Compound addition operator overload.
     inline Point2D<T>& operator+=(const Point2D<T>& p) {
       xp += p.xp;
       yp += p.yp;
       return *this;
     }
 
-    // Compound subtraction operator overload.
+    /// Compound subtraction operator overload.
     inline Point2D<T>& operator-=(const Point2D<T>& p) {
       xp -= p.xp;
       yp -= p.yp;
       return *this;
     }
 
-    // Compound multiplication operator overload.
+    /// Compound multiplication operator overload.
     inline Point2D<T>& operator*=(T c) {
       xp *= c;
       yp *= c;
       return *this;
     }
 
-    // Compound division operator overload.
+    /// Compound division operator overload.
     inline Point2D<T>& operator/=(T divisor) {
       xp /= divisor;
       yp /= divisor;
       return *this;
     }
 
-    // Equality operator overload.
+    /// Equality operator overload.
     friend inline bool operator==(const Point2D<T>& p1, const Point2D<T>& p2) {
       return (p2.xp - p1.xp < 0.001f) && (p2.yp - p1.yp < 0.001f);
     }
 
-    // Inequality operaton overload.
+    /// Inequality operaton overload.
     friend inline bool operator!=(const Point2D<T>& p1, const Point2D<T>& p2) {
       return !(p2.xp - p1.xp < 0.001f) || !(p2.yp - p1.yp < 0.001f);
     }
 
-    // Addition operator overload.
+    /// Addition operator overload.
     friend inline const Point2D<T> operator+(const Point2D<T>& p1,
                                              const Point2D<T>& p2) {
       return Point2D<T>(p1.xp + p2.xp, p1.yp + p2.yp);
     }
 
-    // Subtraction operator overload.
+    /// Subtraction operator overload.
     friend inline const Point2D<T> operator-(const Point2D<T>& p1,
                                              const Point2D<T>& p2) {
       return Point2D<T>(p1.xp - p2.xp, p1.yp - p2.yp);
     }
 
-    // Scalar multiplication operator overload (Point on left side).
+    /// Scalar multiplication operator overload (Point on left side).
     friend inline const Point2D<T> operator*(const Point2D<T>& p, T c) {
       return Point2D<T>(p.xp*c, p.yp*c);
     }
 
-    // Scalar multiplication operator overload (Point on right side).
+    /// Scalar multiplication operator overload (Point on right side).
     friend inline const Point2D<T> operator*(T c, const Point2D<T>& p) {
       return Point2D<T>(p.xp*c, p.yp*c);
     }
 
-    // Unary positive operator overload.
+    /// Unary positive operator overload.
     friend inline const Point2D<T> operator+(const Point2D<T>& p) {
       return p;
     }
 
-    // Unary negate operator overload.
+    /// Unary negate operator overload.
     friend inline const Point2D<T> operator-(const Point2D<T>& p) {
       return Point2D<T>(-p.xp, -p.yp);
     }
 
-    // Division operator overload.
+    /// Division operator overload.
     friend inline const Point2D<T> operator/(const Point2D<T>& p, T divisor) {
       return Point2D<T>(p.xp / divisor, p.yp / divisor);
     }
 
-    // Stream redirection operator overload.
+    /// Stream redirection operator overload.
     friend inline std::ostream& operator<<(std::ostream& os, Point2D<T>& p) {
       return os << "(" << p.xp << ", " << p.yp << ")";
     }
 
-    // Returns distance between two points. [static]
+    /// Returns distance between two points. [static]
     static inline T distance(const Point2D<T>&p1, const Point2D<T>& p2) {
       return std::hypot(p2.xp - p1.xp, p2.yp - p1.yp);
     }
 
-    // Returns distance squared between two points. [static]
+    /// Returns distance squared between two points. [static]
     static inline T distance2(const Point2D<T>& p1, const Point2D<T>& p2) {
       T x = p2.xp - p1.xp;
       T y = p2.yp - p1.yp;
       return x*x + y*y;
     }
 
-    // Returns dot product of two points. [static]
+    /// Returns dot product of two points. [static]
     static inline T dotProduct(const Point2D<T> &p1, const Point2D<T> &p2) {
       return p1.xp * p2.xp + p1.yp * p2.yp;
     }
 
+    /// Draw point with OpenGL calls.
     void draw() const {
       if(smooth)
         glEnable(GL_POINT_SMOOTH);
@@ -171,7 +172,7 @@ namespace Utils {
 
   }; // end class Point2D
 
-  // Type specific OpenGL calls.
+  /// Type specific OpenGL calls.
   template <typename T> void glVertex2(const Point2D<T>& p);
 
   template <> void glVertex2<GLshort>(const Point2D<GLshort>& p) {
