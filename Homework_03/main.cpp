f8af505c486c615ca5f47d2f4db7cb1d8078aea9
Zoltan Dalmadi <dmz985@gmail.com>
2014-10-21 16:14:30 +0200

    Added draw member function

--- Homework_03/main.cpp
+++ Homework_03/main.cpp
@@ -1,25 +1,40 @@
 #include <GL/glut.h>
 #include <iostream>
 #include <cmath>
+#include "Rectangle.h"
 
 // Window size.
 const GLsizei WIDTH = 800;
 const GLsizei HEIGHT = 800;
+const Utils::Color bgColor(Utils::WHITE);
+
+typedef Utils::Rectangle<GLint> Rect;
+
+Rect rect(100, 400, 200, 300);
 
 void init() {
+  bgColor.setGLClearColor();
 
+  // Display settings
+  glMatrixMode(GL_PROJECTION);
+  gluOrtho2D(0.0, WIDTH, 0.0, HEIGHT);
+  glEnable(GL_LINE_SMOOTH);
+  glEnable(GL_POINT_SMOOTH);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void display() {
-  
+  glClear(GL_COLOR_BUFFER_BIT);
+  rect.draw();
+  glutSwapBuffers();
 }
 
 int main(int argc, char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
   glutInitWindowSize(WIDTH, HEIGHT);
-  glutInitWindowPosition(100, 100);
-  glutCreateWindow("Homework 01");
+  glutCreateWindow("Homework 03");
 
   init();
   glutDisplayFunc(display);
--- Utils/Rectangle.h
+++ Utils/Rectangle.h
@@ -10,14 +10,21 @@ template <typename T>
 class Rectangle {
 private:
   Point2D<T> topLeft;
+  Point2D<T> topRight;
   Point2D<T> bottomRight;
+  Point2D<T> bottomLeft;
 
 public:
-  Rectangle(const Point2D<T> &topleft, const Point2D<T> &bottomRight)
-    : topLeft(topleft), bottomRight(bottomRight) {}
+  Color color = RED;
+
+  Rectangle(const Point2D<T> &topleft, const Point2D<T> &topright,
+            const Point2D<T> &bottomright, const Point2D<T> &bottomleft)
+            : topLeft(topleft), topRight(topright),
+            bottomRight(bottomright), bottomLeft(bottomleft) {}
 
   Rectangle(T x, T y, T width, T height)
-    : topLeft(x, y), bottomRight(x + width, y + height) {}
+    : topLeft(x, y), topRight(x + width, y),
+    bottomRight(x + width, y - height), bottomLeft(x, y - height) {}
 
   virtual ~Rectangle() {}
 
@@ -27,6 +34,16 @@ public:
 
   inline T height() {
     return bottomRight.y() - topLeft.y();
+  }
+
+  void draw() {
+    this->color.setGLColor();
+    glBegin(GL_POLYGON);
+    glVertex2<T>(topLeft);
+    glVertex2<T>(topRight);
+    glVertex2<T>(bottomRight);
+    glVertex2<T>(bottomLeft);
+    glEnd();
   }
 
 }; // end class Rectangle
