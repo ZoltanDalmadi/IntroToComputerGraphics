914534e3a071936b06eebb7cba983c179434e3a9
Zoltan Dalmadi <dmz985@gmail.com>
2014-11-02 16:46:20 +0100

    Homework 04 complete, but needs cleanup

--- Homework_04/main.cpp
+++ Homework_04/main.cpp
@@ -27,8 +27,11 @@ const GLfloat lineWidth = 2.0;
 
 // Points ---------------------------------------------------------------------
 Line tangent(100, 150, 300, 550);
-Point2D P3(600, 350);
-Point2D P4(1000, 600);
+Point2D tangent2;
+Point2D P3(300, 350);
+Point2D P4(500, 400);
+Point2D P5(800, 600);
+Point2D P6(1000, 150);
 
 GLdouble minParam = -2.0f;
 GLdouble maxParam = 2.0f;
@@ -36,7 +39,8 @@ GLdouble t1 = -1.0f;
 GLdouble t2 = 0.0f;
 GLdouble t3 = 1.0f;
 
-Matrix3x4 G;
+Matrix3x4 G1;
+Matrix3x4 G2;
 Matrix4x4 preM;
 Matrix4x4 M;
 Vector4x1 T;
@@ -51,16 +55,39 @@ Point2D notch1(sliderLine.pointAt(0.25));
 Point2D notch2(sliderLine.pointAt(0.5));
 Point2D notch3(sliderLine.pointAt(0.75));
 
-void updateGMatrix()
+void calcTangent2()
 {
-  G(0, 0) = tangent.x1();
-  G(1, 0) = tangent.y1();
-  G(0, 1) = P3.x();
-  G(1, 1) = P3.y();
-  G(0, 2) = P4.x();
-  G(1, 2) = P4.y();
-  G(0, 3) = tangent.dx();
-  G(1, 3) = tangent.dy();
+  T(0, 0) = 3*t3*t3;
+  T(1, 0) = 2*t3;
+  T(2, 0) = 1;
+  T(3, 0) = 0;
+  temp = G1 * M * T;
+  tangent2.setX(temp(0, 0));
+  tangent2.setY(temp(1, 0));
+}
+
+void updateG1Matrix()
+{
+  G1(0, 0) = tangent.x1();
+  G1(1, 0) = tangent.y1();
+  G1(0, 1) = P3.x();
+  G1(1, 1) = P3.y();
+  G1(0, 2) = P4.x();
+  G1(1, 2) = P4.y();
+  G1(0, 3) = tangent.dx();
+  G1(1, 3) = tangent.dy();
+}
+
+void updateG2Matrix()
+{
+  G2(0, 0) = P4.x();
+  G2(1, 0) = P4.y();
+  G2(0, 1) = P5.x();
+  G2(1, 1) = P5.y();
+  G2(0, 2) = P6.x();
+  G2(1, 2) = P6.y();
+  G2(0, 3) = tangent2.x();
+  G2(1, 3) = tangent2.y();
 }
 
 void updateMMatrix()
@@ -79,7 +106,7 @@ void updateMMatrix()
 
   // derivative for tangent
   preM(0, 3) = 3.0f * t1 * t1;
-  preM(1, 3) = 2.0f * t1 ;
+  preM(1, 3) = 2.0f * t1;
 
   // we need inverse of all this
   M = preM.inverse();
@@ -99,10 +126,14 @@ void init()
   tangent.lineWidth = lineWidth;
 
   // these parts of the matrices never change
-  G(2, 0) = 1.0;
-  G(2, 1) = 1.0;
-  G(2, 2) = 1.0;
-  G(2, 3) = 1.0;
+  G1(2, 0) = 1.0;
+  G1(2, 1) = 1.0;
+  G1(2, 2) = 1.0;
+  G1(2, 3) = 1.0;
+  G2(2, 0) = 1.0;
+  G2(2, 1) = 1.0;
+  G2(2, 2) = 1.0;
+  G2(2, 3) = 1.0;
   preM(3, 0) = 1.0;
   preM(3, 1) = 1.0;
   preM(3, 2) = 1.0;
@@ -110,8 +141,10 @@ void init()
   preM(3, 3) = 0.0;
 
   // update matrices
-  updateGMatrix();
   updateMMatrix();
+  updateG1Matrix();
+  calcTangent2();
+  updateG2Matrix();
 
   t1Slider.setValue(25);
   t2Slider.setValue(50);
@@ -130,11 +163,19 @@ void display()
   glLineWidth(lineWidth);
   glBegin(GL_LINE_STRIP);
 
-  t1 = minParam + (t1Slider.getValue()*(maxParam-minParam))/100;
-  t2 = minParam + (t2Slider.getValue()*(maxParam-minParam))/100;
-  t3 = minParam + (t3Slider.getValue()*(maxParam-minParam))/100;
-  updateMMatrix();
-  Matrix3x4 C = G * M;
+  Matrix3x4 C = G1 * M;
+  for(GLdouble t = t1; t <= t3; t += 0.01f)
+  {
+    T(0, 0) = t*t*t;
+    T(1, 0) = t*t;
+    T(2, 0) = t;
+    T(3, 0) = 1;
+    temp = C * T;
+    glVertex2d(temp(0, 0), temp(1, 0));
+  }
+
+  C = G2 * M;
+
   for(GLdouble t = t1; t <= t3; t += 0.01f)
   {
     T(0, 0) = t*t*t;
@@ -150,6 +191,8 @@ void display()
   tangent.drawPoints();
   P3.draw();
   P4.draw();
+  P5.draw();
+  P6.draw();
 
   sliderLine.draw();
   sliderLine.drawPoints();
@@ -171,6 +214,8 @@ void processMouse(GLint button, GLint action, GLint xMouse, GLint yMouse)
     tangent.rp2().checkClick(xMouse, HEIGHT - yMouse, 12);
     P3.checkClick(xMouse, HEIGHT - yMouse, 12);
     P4.checkClick(xMouse, HEIGHT - yMouse, 12);
+    P5.checkClick(xMouse, HEIGHT - yMouse, 12);
+    P6.checkClick(xMouse, HEIGHT - yMouse, 12);
     t1Slider.checkClick(xMouse, HEIGHT - yMouse, 12);
     t2Slider.checkClick(xMouse, HEIGHT - yMouse, 12);
     t3Slider.checkClick(xMouse, HEIGHT - yMouse, 12);
@@ -182,6 +227,8 @@ void processMouse(GLint button, GLint action, GLint xMouse, GLint yMouse)
     tangent.rp2().release();
     P3.release();
     P4.release();
+    P5.release();
+    P6.release();
     t1Slider.release();
     t2Slider.release();
     t3Slider.release();
@@ -198,46 +245,78 @@ void processMouseActiveMotion(GLint xMouse, GLint yMouse)
     tangent.rp1().setY(HEIGHT - yMouse);
     tangent.rp2().setX(xMouse + dx);
     tangent.rp2().setY(HEIGHT - yMouse + dy);
-    G(0, 0) = tangent.x1();
-    G(1, 0) = tangent.y1();
+    updateG1Matrix();
+    calcTangent2();
+    updateG2Matrix();
   }
 
   if(tangent.rp2().clicked)
   {
     tangent.rp2().setX(xMouse);
     tangent.rp2().setY(HEIGHT - yMouse);
-    G(0, 0) = tangent.x1();
-    G(1, 0) = tangent.y1();
-    G(0, 3) = tangent.dx();
-    G(1, 3) = tangent.dy();
+    updateG1Matrix();
+    calcTangent2();
+    updateG2Matrix();
   }
 
   if(P3.clicked)
   {
     P3.setX(xMouse);
     P3.setY(HEIGHT - yMouse);
-    G(0, 1) = P3.x();
-    G(1, 1) = P3.y();
+    updateG1Matrix();
+    calcTangent2();
+    updateG2Matrix();
   }
 
   if(P4.clicked)
   {
     P4.setX(xMouse);
     P4.setY(HEIGHT - yMouse);
-    G(0, 2) = P4.x();
-    G(1, 2) = P4.y();
+    updateG1Matrix();
+    calcTangent2();
+    updateG2Matrix();
+  }
+
+  if(P5.clicked)
+  {
+    P5.setX(xMouse);
+    P5.setY(HEIGHT - yMouse);
+    updateG1Matrix();
+    calcTangent2();
+    updateG2Matrix();
+  }
+
+  if(P6.clicked)
+  {
+    P6.setX(xMouse);
+    P6.setY(HEIGHT - yMouse);
+    updateG1Matrix();
+    calcTangent2();
+    updateG2Matrix();
   }
   
   if (t1Slider.isDragging()) {
     t1Slider.setHandlePos(xMouse);
+    t1 = minParam + (t1Slider.getValue()*(maxParam-minParam))/100;
+    calcTangent2();
+    updateG2Matrix();
+    updateMMatrix();
   }
 
   if (t2Slider.isDragging()) {
     t2Slider.setHandlePos(xMouse);
+    t2 = minParam + (t2Slider.getValue()*(maxParam-minParam))/100;
+    calcTangent2();
+    updateG2Matrix();
+    updateMMatrix();
   }
 
   if (t3Slider.isDragging()) {
     t3Slider.setHandlePos(xMouse);
+    t3 = minParam + (t3Slider.getValue()*(maxParam-minParam))/100;
+    calcTangent2();
+    updateG2Matrix();
+    updateMMatrix();
   }
 
   glutPostRedisplay();
