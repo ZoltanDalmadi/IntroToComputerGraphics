be46979b5718e5660fef4126d2bdc99df3a04b84
Zoltan Dalmadi <dmz985@gmail.com>
2014-11-07 17:57:58 +0100

    Homework 05 complete (piece of cake)

--- Homework_05/main.cpp
+++ Homework_05/main.cpp
@@ -1,10 +1,12 @@
-
 #include <GL/glut.h>
 #include "Matrix.h"
-#include "Color.h"
+#include "PolyStar.h"
 
 // Typedefs -------------------------------------------------------------------
 typedef Utils::Matrix<GLdouble> Matrix;
+typedef Utils::Rotate2D<GLdouble> Rotate2D;
+typedef Utils::Scale2D<GLdouble> Scale2D;
+typedef Utils::PolyStar<GLdouble> PolyStar;
 
 // Window size ----------------------------------------------------------------
 const GLsizei WIDTH = 1280;
@@ -15,10 +17,24 @@ const Utils::Color bgColor(Utils::WHITE);
 
 // Sizes ----------------------------------------------------------------------
 const GLfloat lineWidth = 2.0f;
-const int gridSize = 40;
 
 // Refresh rate ---------------------------------------------------------------
-const size_t refreshRate = 5;
+const size_t refreshRate = 20;
+
+// Stars ----------------------------------------------------------------------
+PolyStar star1(WIDTH / 2, HEIGHT / 2, 6, 80, 200);
+PolyStar star2(WIDTH / 2, HEIGHT / 2, 6, 200, 300);
+
+// Matrices -------------------------------------------------------------------
+Rotate2D rot1((2 * Utils::PI) / 360);
+Rotate2D rot2(-(2 * Utils::PI) / 360);
+Scale2D scale1(0.99);
+Scale2D scale2(1/0.99);
+Matrix T1(3, 3);
+Matrix T2(3, 3);
+
+// Frames of animation --------------------------------------------------------
+size_t frames = 0;
 
 void init()
 {
@@ -31,41 +47,40 @@ void init()
   glEnable(GL_POINT_SMOOTH);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-}
 
-void drawGrid(int width, int height, int gap, GLfloat lineWidth,
-              const Utils::Color& color)
-{
-  color.setGLColor();
-  glLineWidth(lineWidth);
-  glEnable(GL_LINE_STIPPLE);
-  glLineStipple(1, 0xAAAA);
-  glBegin(GL_LINES);
-
-  for (int i = gap; i < width; i += gap)
-  {
-    glVertex2i(i, 0);
-    glVertex2i(i, height);
-  }
-
-  for (int i = gap; i < height; i += gap)
-  {
-    glVertex2i(0, i);
-    glVertex2i(width, i);
-  }
-
-  glEnd();
-  glDisable(GL_LINE_STIPPLE);
+  T1 = rot1 * scale1;
+  T2 = rot2 * scale1;
 }
 
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
+  star1.draw();
+  star2.draw();
+
   glutSwapBuffers();
+
+  star1.transform(T1);
+  star2.transform(T2);
 }
 
 void appUpdate(int n)
 {
+  frames++;
+
+  if(frames == 90)
+  {
+    T1 = rot1 * scale2;
+    T2 = rot2 * scale2;
+  }
+
+  if(frames == 180)
+  {
+    T1 = rot1 * scale1;
+    T2 = rot2 * scale1;
+    frames = 0;
+  }
+
   glutPostRedisplay();
   glutTimerFunc(refreshRate, appUpdate, 0);
 }
