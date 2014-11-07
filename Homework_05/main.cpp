1c3f68b36d3e89d88c091294150dc73d676a2020
Zoltan Dalmadi <dmz985@gmail.com>
2014-11-07 18:46:48 +0100

    Thickened star lines

--- Homework_05/main.cpp
+++ Homework_05/main.cpp
@@ -29,7 +29,7 @@ PolyStar star2(WIDTH / 2, HEIGHT / 2, 6, 200, 300);
 Rotate2D rot1((2 * Utils::PI) / 360);
 Rotate2D rot2(-(2 * Utils::PI) / 360);
 Scale2D scale1(0.99);
-Scale2D scale2(1/0.99);
+Scale2D scale2(1 / 0.99);
 Matrix T1(3, 3);
 Matrix T2(3, 3);
 
@@ -50,6 +50,8 @@ void init()
 
   T1 = rot1 * scale1;
   T2 = rot2 * scale1;
+  star1.lineWidth = lineWidth;
+  star2.lineWidth = lineWidth;
 }
 
 void display()
@@ -57,7 +59,6 @@ void display()
   glClear(GL_COLOR_BUFFER_BIT);
   star1.draw();
   star2.draw();
-
   glutSwapBuffers();
 
   star1.transform(T1);
@@ -68,13 +69,13 @@ void appUpdate(int n)
 {
   frames++;
 
-  if(frames == 90)
+  if (frames == 90)
   {
     T1 = rot1 * scale2;
     T2 = rot2 * scale2;
   }
 
-  if(frames == 180)
+  if (frames == 180)
   {
     T1 = rot1 * scale1;
     T2 = rot2 * scale1;
