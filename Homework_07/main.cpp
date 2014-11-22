cc8cc76807d559a6b0b1db713bf044e423b63cad
Zoltan Dalmadi <dmz985@gmail.com>
2014-11-22 22:52:08 +0100

    Interpolation drawing test passed

--- Homework_07/main.cpp
+++ Homework_07/main.cpp
@@ -2,11 +2,13 @@
 #include "Polygon2D.h"
 #include "Line.h"
 #include "Bezier2D.h"
+#include "Slider.h"
 
 // Typedefs -------------------------------------------------------------------
 typedef Utils::Point2D<GLdouble> Point2D;
 typedef Utils::Line<GLdouble> Line;
 typedef Utils::Bezier2D<GLdouble> Bezier2D;
+typedef Utils::Slider Slider;
 
 // Window size ----------------------------------------------------------------
 const GLsizei WIDTH = 1280;
@@ -22,6 +24,8 @@ const GLfloat lineWidth = 2.0f;
 Point2D *clicked = nullptr;
 
 Bezier2D b1;
+
+Slider slider(100, 40, WIDTH - 100, 40);
 
 void init()
 {
@@ -39,7 +43,6 @@ void init()
   //b1.addPoint(300, 700);
   //b1.addPoint(600, 600);
   //b1.addPoint(800, 200);
-  b1.lineWidth = lineWidth;
 }
 
 void display()
@@ -48,7 +51,10 @@ void display()
   b1.drawControlPolygon();
   b1.draw();
   //b1.drawBernstein();
+  b1.drawInterPolations(static_cast<double>(slider.getValue()) / 100.0f);
   b1.drawPoints();
+
+  slider.draw();
 
   glutSwapBuffers();
 }
@@ -57,19 +63,21 @@ void processMouse(GLint button, GLint action, GLint xMouse, GLint yMouse)
 {
   if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN)
   {
-    clicked = b1.checkClick(xMouse, HEIGHT - yMouse, 12);
-    //if (b1.getPoints() < 3)
-    if (!clicked)
+    slider.checkClick(xMouse, HEIGHT - yMouse, 12);
+    if (!slider.isDragging())
     {
-      b1.addPoint(xMouse, HEIGHT - yMouse);
+      clicked = b1.checkClick(xMouse, HEIGHT - yMouse, 12);
+      //if (b1.getPoints() < 3)
+      if (!clicked)
+        b1.addPoint(xMouse, HEIGHT - yMouse);
     }
-    //else
-    //{
-    //}
   }
 
   if (button == GLUT_LEFT_BUTTON && action == GLUT_UP)
   {
+    if (slider.isDragging())
+      slider.release();
+
     if (clicked)
     {
       clicked = nullptr;
@@ -87,10 +95,11 @@ void processMouseActiveMotion(GLint xMouse, GLint yMouse)
   if (clicked)
   {
     if (b1.clicked)
-    {
       b1.handleClick(xMouse, HEIGHT - yMouse, clicked);
-    }
   }
+
+  if(slider.isDragging())
+    slider.setHandlePos(xMouse);
 
   glutPostRedisplay();
 }
