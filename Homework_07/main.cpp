28a770121b720f5af1ca633355233df157792352
Zoltan Dalmadi <dmz985@gmail.com>
2014-11-22 17:40:19 +0100

    Bernstein polynomial test passed

--- Homework_07/main.cpp
+++ Homework_07/main.cpp
@@ -1,10 +1,12 @@
 #include <GL/freeglut.h>
 #include "Polygon2D.h"
 #include "Line.h"
+#include "Bezier2D.h"
 
 // Typedefs -------------------------------------------------------------------
 typedef Utils::Point2D<GLdouble> Point2D;
 typedef Utils::Line<GLdouble> Line;
+typedef Utils::Bezier2D<GLdouble> Bezier2D;
 
 // Window size ----------------------------------------------------------------
 const GLsizei WIDTH = 1280;
@@ -19,6 +21,8 @@ const GLfloat lineWidth = 2.0f;
 // Active points --------------------------------------------------------------
 Point2D *clicked = nullptr;
 
+Bezier2D b1;
+
 void init()
 {
   bgColor.setGLClearColor();
@@ -30,11 +34,19 @@ void init()
   glEnable(GL_POINT_SMOOTH);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  b1.addPoint(200, 300);
+  b1.addPoint(300, 700);
+  b1.addPoint(600, 600);
+  b1.addPoint(800, 200);
+  b1.lineWidth = lineWidth;
 }
 
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
+  b1.drawBernstein();
+  b1.drawPoints();
   glutSwapBuffers();
 }
 
@@ -42,10 +54,20 @@ void processMouse(GLint button, GLint action, GLint xMouse, GLint yMouse)
 {
   if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN)
   {
+    clicked = b1.checkClick(xMouse, HEIGHT - yMouse, 12);
   }
 
   if (button == GLUT_LEFT_BUTTON && action == GLUT_UP)
   {
+    if (clicked)
+    {
+      clicked = nullptr;
+
+      if (b1.clicked)
+      {
+        b1.release();
+      }
+    }
   }
 }
 
@@ -53,6 +75,10 @@ void processMouseActiveMotion(GLint xMouse, GLint yMouse)
 {
   if (clicked)
   {
+    if (b1.clicked)
+    {
+      b1.handleClick(xMouse, HEIGHT - yMouse, clicked);
+    }
   }
 
   glutPostRedisplay();
