0934cccae88e33b1bd9e7e4ca3bdc328be3acfb0
Zoltan Dalmadi <dmz985@gmail.com>
2014-11-24 14:15:00 +0100

    Cleanup

--- Utils/Bezier2D.h
+++ Utils/Bezier2D.h
@@ -1,32 +1,13 @@
 #pragma once
 
 #include <GL/freeglut.h>
-#include <cmath>
+#include "functions.h"
 #include "Point2D.h"
 #include "Line.h"
 #include "Color.h"
 
 namespace Utils
 {
-
-// ----------------------------------------------------------------------------
-// Calculate binomial coefficient [recursive]
-// ----------------------------------------------------------------------------
-size_t binomialCoeff(size_t n, size_t k)
-{
-  if (k == 0 || k == n)
-    return 1;
-
-  return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);
-}
-
-// ----------------------------------------------------------------------------
-// Returns value of the Bernstein polynomial.
-// ----------------------------------------------------------------------------
-double bernsteinPolynomial(size_t n, size_t i, double t)
-{
-  return binomialCoeff(n, i) * std::pow(t, i) * std::pow(1 - t, n - i);
-}
 
 // ----------------------------------------------------------------------------
 // 2D Bezier curve class template
