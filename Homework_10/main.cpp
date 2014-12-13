ab5fd44d6b3add2c955eb0391e8dd88e6fd1835c
Zoltan Dalmadi <dmz985@gmail.com>
2014-12-13 13:56:29 +0100

    First test

--- Homework_10/main.cpp
+++ Homework_10/main.cpp
@@ -4,16 +4,20 @@
 #include "Point3D.h"
 #include "Color.h"
 #include "Vector2D.h"
+#include "Sphere.h"
 
 // ----------------------------------------------------------------------------
 // Typedefs
 // ----------------------------------------------------------------------------
 typedef Utils::Rectangle<GLdouble> Rect;
-typedef Utils::CentralProjection<GLdouble> CP;
+typedef Utils::CentralProjection<GLdouble> CentralProjection;
 typedef Utils::WindowToViewport<GLdouble> WTV;
+typedef Utils::Rotate3DX<GLdouble> Rotate3DX;
+typedef Utils::Rotate3DY<GLdouble> Rotate3DY;
 typedef Utils::Point2D<GLdouble> Point2D;
 typedef Utils::Point3D<GLdouble> Point3D;
 typedef Utils::Point3DH<GLdouble> Point3DH;
+typedef Utils::Sphere<GLdouble> Sphere;
 
 // ----------------------------------------------------------------------------
 // Window size
@@ -37,27 +41,31 @@ const size_t refreshRate = 10;
 // Sizes
 // ----------------------------------------------------------------------------
 const GLfloat lineWidth = 2.0f;
-const int gridSize = 40;
 
 // ----------------------------------------------------------------------------
 // Miscellaneous variables
 // ----------------------------------------------------------------------------
-const double step = 0.5f;
-const double xMin = -20.0f;
-const double xMax = 20.0f;
-const double yMin = -20.0f;
-const double yMax = 20.0f;
+bool drag = false;
+double lastRotX = 0.0f;
+double lastRotY = 0.0f;
+GLint clickedX;
+GLint clickedY;
 
 // ----------------------------------------------------------------------------
 // Window and viewport
 // ----------------------------------------------------------------------------
-Rect window(xMin, yMin, xMax, yMax);
+Rect window(-1.5, -1.5, 1.5, 1.5);
 Rect viewport(280, 0, 280 + HEIGHT, HEIGHT);
 
 // ----------------------------------------------------------------------------
 // Matrices
 // ----------------------------------------------------------------------------
-WTV wtv1(window, viewport);
+CentralProjection cp(8.0f);
+WTV wtv(window, viewport);
+Rotate3DX rx(0);
+Rotate3DY ry(0);
+
+Sphere sphere;
 
 // ----------------------------------------------------------------------------
 // Init function
@@ -71,6 +79,7 @@ void init()
   glEnable(GL_POINT_SMOOTH);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  sphere.pointSize = 6.0f;
 }
 
 // ----------------------------------------------------------------------------
@@ -80,7 +89,84 @@ void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
 
+  auto projTrans = wtv * cp;
+  auto rxry = rx * ry;
+  auto T = projTrans * rxry;
+
+  sphere.drawEdges(T);
+  //sphere.drawPoints(T);
+
   glutSwapBuffers();
+}
+
+// ----------------------------------------------------------------------------
+// Main mouse button handler
+// ----------------------------------------------------------------------------
+void processMouse(GLint button, GLint action, GLint xMouse, GLint yMouse)
+{
+  if (button == GLUT_LEFT_BUTTON && action == GLUT_DOWN)
+  {
+    drag = true;
+
+    // cache clicked point position
+    clickedX = xMouse;
+    clickedY = HEIGHT - yMouse;
+  }
+
+  if (button == GLUT_LEFT_BUTTON && action == GLUT_UP)
+  {
+    drag = false;
+
+    // cache current rotation values
+    lastRotX = rx.getAngle();
+    lastRotY = ry.getAngle();
+
+    glutPostRedisplay();
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Main mouse movement handler
+// ----------------------------------------------------------------------------
+void processMouseActiveMotion(GLint xMouse, GLint yMouse)
+{
+  if (drag)
+  {
+    // calculate vector between clicked and dragged point
+    Utils::Vector2D<GLint> v(clickedX, clickedY, xMouse, HEIGHT - yMouse);
+
+    // set rotation values
+    rx.setAngle(lastRotX - Utils::degToRad(v.y()) * 0.25);
+    ry.setAngle(lastRotY + Utils::degToRad(v.x()) * 0.25);
+  }
+
+  glutPostRedisplay();
+}
+
+void keyPressed(int key, int x, int y)
+{
+  if (key == GLUT_KEY_UP)
+  {
+    auto old = cp.getDistanceToOrigin();
+    cp.setDistanceToOrigin(old - 0.1f);
+  }
+
+  if (key == GLUT_KEY_DOWN)
+  {
+    auto old = cp.getDistanceToOrigin();
+    cp.setDistanceToOrigin(old + 0.1f);
+  }
+
+  glutPostRedisplay();
+}
+
+// ----------------------------------------------------------------------------
+// Mouse wheel handler
+// ----------------------------------------------------------------------------
+void wheelFunc(int wheel, int direction, int x, int y)
+{
+  direction > 0 ? sphere++ : sphere--;
+  glutPostRedisplay();
 }
 
 // ----------------------------------------------------------------------------
@@ -95,6 +181,10 @@ int main(int argc, char **argv)
 
   init();
   glutDisplayFunc(display);
+  glutMouseFunc(processMouse);
+  glutMotionFunc(processMouseActiveMotion);
+  glutSpecialFunc(keyPressed);
+  glutMouseWheelFunc(wheelFunc);
   glutMainLoop();
   return 0;
 }
