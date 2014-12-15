77e0b6b53d71fa7bb586bd1db69e93d9c3b4ec6c
Zoltan Dalmadi <dmz985@gmail.com>
2014-12-15 01:02:38 +0100

    Enhanced label padding possibilities

--- Utils/Button.h
+++ Utils/Button.h
@@ -12,7 +12,8 @@ class Button
 private:
   Rectangle<GLint> body;
   std::string label;
-  size_t labelPadding = 10;
+  size_t paddingX = 10;
+  size_t paddingY = 10;
   Color color = BLACK;
 
 public:
@@ -28,7 +29,27 @@ public:
     this->body.color = color;
   }
 
-  bool hover(const GLint& x, const GLint& y) const
+  inline size_t getPaddingY() const
+  {
+    return paddingY;
+  }
+
+  inline void setPaddingY(const size_t &value)
+  {
+    paddingY = value;
+  }
+
+  inline size_t getPaddingX() const
+  {
+    return paddingX;
+  }
+
+  inline void setPaddingX(const size_t &value)
+  {
+    paddingX = value;
+  }
+
+  inline bool hover(const GLint& x, const GLint& y) const
   {
     return body.isInside(x, y);
   }
@@ -38,10 +59,11 @@ public:
     body.draw();
     color.setGLColor();
 
-    glRasterPos2i(body.left() + labelPadding, body.bottom() + labelPadding);
+    glRasterPos2i(body.left() + paddingX, body.bottom() + paddingY);
     glutBitmapString(GLUT_BITMAP_HELVETICA_18, (unsigned char *)label.c_str());
   }
+};
 
-}; // end class Button
+// end class Button
 
 } // end namespace Utils
